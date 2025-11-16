11002eaac7e53e5c200009221dedd246d73287a3
Basavaraj <bgoudar25@gmail.com>
2025-11-16 14:17:01 +0530

    added python file

---
+++ Python/Sample_code.py
@@ -0,0 +1,9 @@
+# from calculator import div  # Tries to import 'add' function from 'calculator.py'
+
+# import calculator
+# from calculator import add, sub, div
+# from calculator import *
+a = 10
+b = 20
+result = div(a, b)          # Calls the 'add' function with 10 and 2
+print(result)               # Prints the result
---
+++ Python/calculator.py
@@ -0,0 +1,10 @@
+# def add(a, b):
+#     result= a + b
+def add(a, b):
+    return a + b
+
+def sub(a ,b):
+    return a - b
+
+def div(a, b):
+    return a/b
